9693fee22ce26ead5dd2e79f5f79da991fa0c618
Wesley de Groot <email@wesleydegroot.nl>
2024-09-21 17:15:35 +0200

    Fix build

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OnboardingKit",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v15),
         .macOS(.v12),
@@ -21,7 +22,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OnboardingKit"),
+            name: "OnboardingKit",
+            resources: [
+                .process("Localizable.xcstrings")
+            ]
+        ),
         .testTarget(
             name: "OnboardingKitTests",
             dependencies: ["OnboardingKit"])
